aed09c01edaaf8ba94356991d6c322555c2e9e73
clebim <matheuscleber1998@gmail.com>
2021-05-19 19:57:04 -0300

    Add routes.json

--- src/config/ConnectionDatabaseConfig.ts
+++ src/config/ConnectionDatabaseConfig.ts
@@ -1,12 +1,1 @@
-// eslint-disable-next-line import/no-mutable-exports
-const connection = 'default';
-
-// if (process.env.NODE_ENV === 'test') {
-//   connection = 'test';
-// } else if (process.env.NODE_ENV === 'production') {
-//   connection = 'production';
-// } else {
-//   connection = 'default';
-// }
-
-export default connection;
+export default process.env.NODE_ENV === 'test' ? 'tests' : 'default';
